4cb9847d046165ae387cf9b20940b8e754a801d0
Перелыгина Анастасия Сегеевна <aperelygina@alfabank.ru>
2022-05-17 21:33:19 +0300

    Add apache-commons as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     testImplementation ("org.seleniumhq.selenium:selenium-java:4.1.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    implementation("commons-io:commons-io:2.6")
 }
 
 tasks.test {
